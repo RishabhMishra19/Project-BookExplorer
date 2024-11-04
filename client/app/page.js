728a6d3aff22ebbdc3b7a07bb55ef7af0570ce51
Rishabh Mishra <rishabhmishra@Rishabhs-MacBook-Pro.local>
2024-11-04 23:19:19 +0530

    added key

--- client/app/page.js
+++ client/app/page.js
@@ -12,6 +12,7 @@ export default function Home() {
         <div className="flex flex-wrap justify-between">
           {[1, 2, 3, 4, 5, 6].map((val) => (
             <BookCard
+              key={val}
               book={{
                 title: "To Kill a Mockingbird",
                 author: "Harper Lee",
