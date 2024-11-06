f65a4f0cbe82621828c8922a56b514e359722bad
Rishabh Mishra <rishabhmishra@Rishabhs-MacBook-Pro.local>
2024-11-06 13:51:50 +0530

    update book form added

--- client/app/books/[bookId]/update/page.js
+++ client/app/books/[bookId]/update/page.js
@@ -1,12 +1,95 @@
+"use client";
+
 import Link from "next/link";
 import { BookForm } from "../../../../components/BookForm";
+import { gql, useMutation, useQuery } from "@apollo/client";
+import { use } from "react";
+import { useRouter } from "next/navigation";
+import { GenericLoader } from "../../../../components/GenericLoader";
+import { GenericError } from "../../../../components/GenericError";
+import toast, { Toaster } from "react-hot-toast";
 
-export default function Home() {
+const QUERY = gql`
+  query getBookById($getBookByIdId: ID!) {
+    getBookById(id: $getBookByIdId) {
+      id
+      title
+      description
+      published_date
+      author {
+        id
+        name
+      }
+    }
+  }
+`;
+
+const UPDATE_BOOK = gql`
+  mutation UpdateBook($id: ID!, $payload: UpdateBookPayload) {
+    updateBook(id: $id, payload: $payload) {
+      id
+    }
+  }
+`;
+
+export default function Home({ params }) {
+  const unwrappedParams = use(params);
+  const { bookId } = unwrappedParams;
+  const router = useRouter();
+
+  const getBookMetaData = useQuery(QUERY, {
+    variables: { getBookByIdId: bookId, skip: !bookId },
+  });
+
+  const book = getBookMetaData?.data?.getBookById ?? {};
+
+  const [updateBook, updateBookMetaData] = useMutation(UPDATE_BOOK);
+
+  const handleSubmit = (payload) => {
+    updateBook({
+      variables: {
+        payload,
+        id: book.id,
+      },
+    })
+      .then(() => {
+        router.push("/books");
+        toast.success("Successfully Created!");
+      })
+      .catch((e) => {
+        toast.error(
+          updateBookMetaData?.error?.cause?.message ?? "Something went wrong"
+        );
+      });
+  };
+
+  if (getBookMetaData.loading) {
+    return <GenericLoader />;
+  }
+
+  if (getBookMetaData.error) {
+    return (
+      <GenericError
+        message={getBookMetaData.error.cause?.message ?? "Something went wrong"}
+      />
+    );
+  }
+
   return (
     <div className="w-full flex items-center justify-center py-16">
       <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
         <h1 className="text-2xl font-bold text-teal-800 mb-6">Update Book</h1>
-        <BookForm isUpdate={true} />
+        <BookForm
+          initialData={{
+            title: book.title,
+            description: book.description,
+            published_date: book.published_date.split("T")[0],
+            author_id: book.author?.id,
+          }}
+          isUpdate={true}
+          onSubmit={handleSubmit}
+          isSubmitting={updateBookMetaData.loading}
+        />
         <Link
           href="/books"
           className="block mt-4 text-center text-teal-600 hover:underline"
@@ -14,6 +97,7 @@ export default function Home() {
           Back to Home
         </Link>
       </div>
+      <Toaster position="bottom-center" toastOptions={{ duration: 1300 }} />
     </div>
   );
 }
