09d3c5de18b77a0bd2fbcc275a3f36966bbdc84c
Rishabh Mishra <rishabhmishra@Rishabhs-MacBook-Pro.local>
2024-11-07 18:18:18 +0530

    attempt 2 for fxing dynamic routing

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,16 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isDev = process.env.NODE_ENV === "development";
 
 const nextConfig = {
   async rewrites() {
-    return isDev
-      ? [
-          {
-            source: "/api/:path*",
-            destination: "http://localhost:4000/api/:path*", // Proxy to backend in dev mode only
-          },
-        ]
-      : [];
+    return [];
   },
 };
 
