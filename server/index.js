b546cfc4366a1b61683f09abb14963319319b940
Rishabh Mishra <rishabhmishra@Rishabhs-MacBook-Pro.local>
2024-11-08 11:56:39 +0530

    exported express app

--- server/index.js
+++ server/index.js
@@ -8,30 +8,34 @@ const cors = require("cors");
 const { expressMiddleware } = require("@apollo/server/express4");
 const { ApolloServer } = require("@apollo/server");
 
-const createExpressApp = async () => {
-  const app = express();
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: () => {
-      return {
-        authorLoader,
-        bookLoader,
-      };
-    },
-  });
-  await server.start();
-  console.log("Appollo server started successfully");
-  await db.sequelize.authenticate();
-  console.log("Database authenticated successfully");
-  app.use("/graphql", cors(), express.json(), expressMiddleware(server));
-  return app;
-};
+const app = express();
 
-createExpressApp()
-  .then((app) =>
+const createExpressApp = async () => {
+  try {
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+      context: () => {
+        return {
+          authorLoader,
+          bookLoader,
+        };
+      },
+    });
+    await server.start();
+    console.log("Appollo server started successfully");
+    await db.sequelize.authenticate();
+    console.log("Database authenticated successfully");
+    app.use("/graphql", cors(), express.json(), expressMiddleware(server));
     app.listen(4000, () => {
       console.log("Backend is listening on port: 4000");
-    })
-  )
-  .catch((error) => console.log(`Some error occured: ${error}`));
+    });
+    return app;
+  } catch (error) {
+    console.log(`Some error occured: ${error}`);
+  }
+};
+
+createExpressApp();
+
+module.exports = app;
